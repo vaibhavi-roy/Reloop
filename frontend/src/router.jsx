6ff0068857f6709eecaa4b9b28bb8a09c1e3a9ac
Devleena Das <devleena2003@gnmail.com>
2024-08-16 18:19:11 +0530

    final commit

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,35 +3,40 @@ import Home from "./Pages/Home";
 import Auth from "./Pages/Auth";
 import Return from "./Pages/Return";
 import Exchange from "./Pages/Exchange";
+import Tracker from "./Pages/Tracker";
 // import { RouteObject } from "react-router-dom";
 
 const routeConfig = [
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{
-				path: "/auth",
-				element: <Auth />,
-			},
-			{
-				path: "/home",
-				element: <Home />,
-			},
-			{
-				path: "/return",
-				element: <Return />,
-			},
-			{
-				path: "/exchange",
-				element: <Exchange />,
-			},
-			{
-				path: "*",
-				element: <>{"Route doesn't exists"}</>,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/return",
+        element: <Return />,
+      },
+      {
+        path: "/exchange",
+        element: <Exchange />,
+      },
+      {
+        path: "/tracker",
+        element: <Tracker />,
+      },
+      {
+        path: "*",
+        element: <>{"Route doesn't exists"}</>,
+      },
+    ],
+  },
 ];
 
 export default routeConfig;
